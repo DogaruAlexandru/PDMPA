60eacc5bbd1536523a6428984d13b3f38981ea17
vladut0609 <vladut.ichim2000@gmail.com>
2024-01-30 18:10:48 +0200

    Added extended view functionality.

--- Client/app/src/main/java/com/example/client/ui/recipes/RecipesFragment.java
+++ Client/app/src/main/java/com/example/client/ui/recipes/RecipesFragment.java
@@ -1,9 +1,11 @@
 package com.example.client.ui.recipes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +18,7 @@ import com.example.client.data.RecyclerViewInterface;
 import com.example.client.data.model.Recipe;
 import com.example.client.databinding.FragmentContainersBinding;
 import com.example.client.databinding.FragmentRecipesBinding;
+import com.example.client.ui.recipes.recipe.RecipeActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +28,8 @@ import kotlin.collections.ArrayDeque;
 public class RecipesFragment extends Fragment implements RecyclerViewInterface {
 
     private FragmentRecipesBinding binding;
+    // For testing
+    private List<Recipe> recipes;
 
     @Nullable
     @Override
@@ -33,7 +38,18 @@ public class RecipesFragment extends Fragment implements RecyclerViewInterface {
         View root = binding.getRoot();
         RecyclerView recyclerView = root.findViewById(R.id.recipesview);
 
-        List<Recipe> recipes= new ArrayList<Recipe>();
+        // TODO: Remove this after connection with DB is made
+        initRecipeList();
+        // getUserRecipes();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
+        recyclerView.setAdapter(new RecipesAdapter(getContext(), recipes, this));
+        return root;
+    }
+
+    // TODO: Remove after connection with DB is made
+    public void initRecipeList(){
+        recipes = new ArrayList<Recipe>();
         List<String> ingredients = new ArrayList<String>();
         List<String> ingredients2 = new ArrayList<String>();
         ingredients.add("ing1");
@@ -41,18 +57,22 @@ public class RecipesFragment extends Fragment implements RecyclerViewInterface {
         ingredients2.add("ingredient1");
         ingredients2.add("ingredient2");
         recipes.add(new Recipe(2L, "Tort", ingredients, "Tort"));
-        recipes.add(new Recipe(2L, "Salam de biscuiti", ingredients2, ""));
-        recipes.add(new Recipe(2L, "Supa", ingredients, ""));
+        recipes.add(new Recipe(3L, "Salam de biscuiti", ingredients2, ""));
+        recipes.add(new Recipe(4L, "Supa", ingredients, ""));
         ingredients.add("new");
-        recipes.add(new Recipe(2L, "Omleta", ingredients2, ""));
+        recipes.add(new Recipe(5L, "Omleta", ingredients2, ""));
+    }
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
-        recyclerView.setAdapter(new RecipesAdapter(getContext(), recipes));
-        return root;
+    public void getUserRecipes(){
+        //TODO: Add after connection with DB is made
     }
 
     @Override
     public void onItemClick(int pos) {
-
+        Long selectedRecipeId = recipes.get(pos).id();
+        Intent intent = new Intent (requireContext(), RecipeActivity.class);
+        intent.putExtra("type", "view");
+        intent.putExtra("recipe_id", selectedRecipeId);
+        startActivity(intent);
     }
 }
